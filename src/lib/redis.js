67e63498339c180c7dc049fae52772c48bc89bea
wendel <capauzumaki@gmail.com>
2026-01-28 08:07:06 -0300

    Ajustando portas

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -1,6 +1,6 @@
 import Redis from 'ioredis';
 
-const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6379');
+const redis = new Redis(process.env.REDIS_URL || 'redis://localhost:6380');
 
 redis.on('error', (err) => console.error('Erro no Redis:', err));
 
